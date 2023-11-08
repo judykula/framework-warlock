f84d0fdef15da505e393b1b8d5472ced7c463e67
jiangwanyu <jiangwanyu@yanxiu.com>
2023-11-08 16:39:45 +0800

    init

---
+++ src/main/java/com/jwy/warlock/MyJdbcAutoConfiguration.java
@@ -0,0 +1,27 @@
+/*
+ * easy come, easy go.
+ *
+ * contact : syiae.jwy@gmail.com
+ *
+ * · · · · ||   ..     __       ___      ____  ®
+ * · · · · ||  ||  || _ ||   ||    ||   ||      ||
+ * · · · · ||  ||  \\_ ||_.||    ||   \\_  ||
+ * · · _//                                       ||
+ * · · · · · · · · · · · · · · · · · ·· ·    ___//
+ */
+package com.jwy.warlock;
+
+/**
+ * <p>
+ *     jdbc装配入口
+ * </p>
+ *
+ * @author Jiang Wanyu
+ * @version 1.0
+ * @date 2023/11/8
+ */
+public class MyJdbcAutoConfiguration {
+
+    // DO Nothing
+
+}
